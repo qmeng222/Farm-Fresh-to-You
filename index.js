2c9b6723f9325a142b44e2b441cd83d047e3ed91
Qingying Meng <qmeng222@gmail.com>
2022-12-04 23:19:22 -0800

    filled the templates

--- index.js
+++ index.js
@@ -7,56 +7,55 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-// // 7. test msg:
-// const greet = "Hi Meng!";
-// console.log(greet);
-
-//////////////// FILES ///////////////////
-// // 8. reading and writing files:
-// const textIn = fs.readFileSync("starter/txt/input.txt", "utf-8");
-// console.log(textIn);
-// const textOut = `About avocado: ${textIn}\nCreated on ${Date.now()}`;
-// console.log(textOut);
-// fs.writeFileSync("starter/txt/output.txt", textOut);
-// console.log("File was created and written!");
-
-// // 10. reading and writing files asynchronously:
-// fs.readFile("starter/txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("starter/txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile(
-//         "starter/txt/final.txt",
-//         `${data2}\n${data3}`,
-//         "utf-8",
-//         (err) => {
-//           console.log("Your file has been written😀");
-//         }
-//       );
-//     });
-//   });
-// });
-// console.log("Will read file!");
-// // Will read file!
-// // read-this
-
 //////////////// SERVER ///////////////////
-// read the file synchronously:
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
 
-// 11. create web server:
+// read the files synchronously:
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data); // convert text into JS object
+
+// helper function:
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+  // mutate the output after it has been created:
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  if (!product.organic)
+    // .not-organic {display: none;} in template-product.html:
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
 // created server and passed in a callback function that is executed each time that a new request hits the server:
 const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  // res.end("Hello from the server!");
-
   const pathName = req.url;
 
   // Overview:
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is OVERVIEW");
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
 
     // Product:
   } else if (pathName === "/product") {
